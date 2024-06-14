6690c27ae3dc7e261ea32b4df18c0c01a9cbf324
Farel <daydreamer9422@gmail.com>
2024-06-14 16:44:58 +0700

    fix?

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,6 +1,12 @@
 module.exports = {
-  root: true,
-  extends: ['plugin:@next/next/recommended', '@payloadcms'],
-  ignorePatterns: ['**/payload-types.ts'],
-  plugins: ['prettier'],
+  "root": true,
+  "extends": [
+    "plugin:@next/next/recommended",
+    "@payloadcms"
+  ],
+  "ignorePatterns": ["**/payload-types.ts"],
+  "plugins": ["prettier", "simple-import-sort"],
+  "rules": {
+    "simple-import-sort/imports": "error"
+  }
 }
